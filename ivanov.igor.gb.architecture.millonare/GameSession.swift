2371c7b0278e14607574592e94d5d446d23ecc64
Igor Ivanov <gugalaps@gmail.com>
2020-09-03 13:19:02 +0300

    hw#0.1

--- ivanov.igor.gb.architecture.millonare/GameSession.swift
+++ ivanov.igor.gb.architecture.millonare/GameSession.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+final class GameSession {
+    
+    private var vc: GameViewController?
+    
+    // game session states:
+    private var questionsPassedAmount: Int = 0 // кол-во отвеченных вопросов
+    private var questionsFullAmount: Int = 0 // всего вопросов
+    private var hintUsedAuditory: Bool = false // помощь зала
+    private var hintUsedCallFriend: Bool = false // звонок другуы
+    private var hintUsedFiftyPercent: Bool = false // 50x50
+    private var winAmount: Int = 0 // сумма выигрыша
+    
+    
+    public func setViewController(vc: GameViewController) {
+        self.vc = vc
+    }
+}
+
+//MARK:- ReadableGameSessionProtocolDelegate
+extension GameSession: ReadableGameSessionProtocolDelegate {
+    
+    func getFullQuestionsAmount() -> Int {
+        return questionsFullAmount
+    }
+    
+    func getPassedQuestionsAmount() -> Int {
+        return questionsPassedAmount
+    }
+}
+
+
+//MARK:- GameSessionProtocolDelegate
+extension GameSession: GameSessionProtocolDelegate {
+
+    func setQuestionsAmount(_ questionsAmount: Int) {
+        self.questionsFullAmount = questionsAmount
+    }
+    
+    func didSelectRightAnswer() {
+        questionsPassedAmount += 1
+    }
+    
+    func didPressHintAuditory() {
+        hintUsedAuditory = true
+    }
+    
+    func didPressHintCallFriend() {
+        hintUsedCallFriend = true
+    }
+    
+    func didPressFiftyPercent() {
+        hintUsedFiftyPercent = true
+    }
+    
+    func didGameFinish(winAmount: Int) {
+        self.winAmount = winAmount
+    }
+}
--- ivanov.igor.gb.architecture.millonare/Model/GameResult.swift
+++ ivanov.igor.gb.architecture.millonare/Model/GameResult.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+final class GameResult: Codable{
+    
+    var questionsPassedAmount: Int = 0
+    var questionsFullAmount: Int = 0
+    
+    
+    init(questionsPassedAmount: Int, questionsFullAmount: Int) {
+        self.questionsPassedAmount = questionsPassedAmount
+        self.questionsFullAmount = questionsFullAmount
+    }
+    
+    
+    //MARK:- Codable >>
+    enum CodingKeys: String, CodingKey {
+        case questionsPassedAmount
+        case questionsFullAmount
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(questionsPassedAmount, forKey: .questionsPassedAmount)
+        try container.encode(questionsFullAmount, forKey: .questionsFullAmount)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        questionsPassedAmount = try container.decode(Int.self, forKey: .questionsPassedAmount)
+        questionsFullAmount = try container.decode(Int.self, forKey: .questionsFullAmount)
+    }
+}
