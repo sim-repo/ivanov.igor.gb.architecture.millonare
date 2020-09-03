d7e0887e6854275a41417fdf7130b06a22bf137d
Igor Ivanov <gugalaps@gmail.com>
2020-09-03 10:26:01 +0300

    init

---
+++ ivanov.igor.gb.architecture.millonare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ivanov.igor.gb.architecture.millonare
+//
+//  Created by Igor Ivanov on 03.09.2020.
+//  Copyright © 2020 Igor Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
